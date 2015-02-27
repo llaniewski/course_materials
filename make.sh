38a28b4fd8fc1f1922b4a4d54c5f9ffdd94875b8
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2015-02-27 14:23:22 +0100

    deploying all pdfs

--- make.sh
+++ make.sh
@@ -8,12 +8,15 @@ fi
 TEX=pdflatex
 #TEX=/home/llaniewski/tex_test/aa/bin/x86_64-linux/pdflatex
 mkdir -p aux
+mkdir -p pdf
 export AUX=$PWD/aux
 
 ALL=$(find -name "*.tex" -not -path "./tex/*")
 for i in $ALL
 do
 	DIR=$(dirname $i)
+	FILE=$(basename $i)
+	PDF=${FILE%.*}.pdf
 	echo Making $i
 	mkdir -p $AUX/$DIR
 	if ! ((cd $DIR; $TEX -interaction=nonstopmode -halt-on-error -output-directory=$AUX/$DIR $(basename $i)) >error.log 2>&1)
@@ -21,6 +24,15 @@ do
 		cat error.log
 		echo Stoped on error at $i
 		exit -1;
+	else
+		echo "-$AUX/$DIR/$PDF-"
+		if [ -f "$AUX/$DIR/$PDF" ]
+		then
+			mv $AUX/$DIR/$PDF pdf
+		else
+			echo pdf not created at $i
+			exit -2;
+		fi
 	fi
 done
 rm error.log
