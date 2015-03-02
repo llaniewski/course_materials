40e8e92d364f73a1adaa82afc892f8e10b4d2d0f
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2015-03-02 11:40:12 +0100

    New compilation scripts

--- make.sh
+++ make.sh
@@ -1,37 +1,14 @@
-export TEXINPUTS=$PWD/common:
+#!/bin/bash
 
-if [ -d tex/main/bin ]
-then
-	export PATH=$PWD/tex/main/bin/x86_64-linux:$PATH
-fi
+SCRDIR=$PWD/$(dirname $0)
 
-TEX=pdflatex
-#TEX=/home/llaniewski/tex_test/aa/bin/x86_64-linux/pdflatex
-mkdir -p aux
-mkdir -p pdf
-export AUX=$PWD/aux
 
 ALL=$(find -name "*.tex" -not -path "./tex/*")
 for i in $ALL
 do
-	DIR=$(dirname $i)
-	FILE=$(basename $i)
-	PDF=${FILE%.*}.pdf
-	echo Making $i
-	mkdir -p $AUX/$DIR
-	if ! ((cd $DIR; $TEX -interaction=nonstopmode -halt-on-error -output-directory=$AUX/$DIR $(basename $i)) >error.log 2>&1)
+	if ! $SCRDIR/textify.sh $i
 	then
-		cat error.log
 		echo Stoped on error at $i
 		exit -1;
-	else
-		if [ -f "$AUX/$DIR/$PDF" ]
-		then
-			mv $AUX/$DIR/$PDF pdf
-		else
-			echo pdf not created at $i
-			exit -2;
-		fi
 	fi
 done
-rm error.log
---
+++ textify.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+SCRDIR=$(readlink -f $0)
+SCRDIR=$(dirname $SCRDIR)
+
+IN=$1
+if [ "x$IN" == "x" ]
+then
+	echo No input provided
+	echo usage: textify.sh file.tex
+	exit -1;
+fi
+
+#echo SCRDIR:$SCRDIR
+export TEXINPUTS=$SCRDIR/common:
+
+if [ -d $SCRDIR/tex/main/bin ]
+then
+	export PATH=$SCRDIR/tex/main/bin/x86_64-linux:$PATH
+fi
+
+TEX=pdflatex
+mkdir -p $SCRDIR/aux
+mkdir -p $SCRDIR/pdf
+export AUX=$SCRDIR/aux
+
+DIR=$(dirname $IN)
+FILE=$(basename $IN)
+PDF=${FILE%.*}.pdf
+OUT=$SCRDIR/pdf/$PDF
+if [ $OUT -nt $IN ]
+then
+	echo Omitting $IN
+	exit 0;
+fi
+echo Textify $IN
+mkdir -p $AUX/$DIR
+
+cd $DIR
+
+#echo AUX: $AUX
+#echo DIR: $DIR
+#echo TEX: $TEX
+#echo FILE: $FILE
+#echo PDF: $PDF
+#echo OUT: $OUT
+#echo IN: $IN
+
+	if ! $TEX -interaction=nonstopmode -halt-on-error -output-directory=$AUX/$DIR $FILE >error.log 2>&1
+	then
+		cat error.log
+		echo Stoped on error at $IN
+		exit -1;
+	else
+		$TEX -interaction=nonstopmode -halt-on-error -output-directory=$AUX/$DIR $FILE >error.log 2>&1
+		$TEX -interaction=nonstopmode -halt-on-error -output-directory=$AUX/$DIR $FILE >error.log 2>&1
+		if [ -f "$AUX/$DIR/$PDF" ]
+		then
+			mv $AUX/$DIR/$PDF $OUT
+		else
+			echo pdf not created at $IN
+			exit -2;
+		fi
+	fi
+rm error.log
+exit 0;
