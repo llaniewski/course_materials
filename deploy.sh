78f1ff3c13b30368a8eeba38aec364b300e6702b
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2016-03-24 16:45:46 +0100

    Added author to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,13 +15,18 @@ then
 		exit -1
 	fi
 	echo Deploying to GitHub Pages ...
+	name=$(git --no-pager show -s --format="%aN")
+	email=$(git --no-pager show -s --format="%ae")
+	echo Today\'s commit belongs to "$name" \("$email"\)
 	echo Cloning pages
 	git clone --depth 1 --branch gh-pages https://$GH_TOKEN@github.com/ccfd/course_materials.git deploy
 	cp pdf/*.pdf deploy/pdf/
 	pushd deploy
+        git config user.email "$email"
+	git config user.name "$name"
 	git add pdf/*.pdf
 	git commit -m "autodeploy to pages"
-	git push
+#	git push
 	popd
 	rm -fr deploy
 else
