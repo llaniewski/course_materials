5e9411b6851b20e10b3681bb52c2daf32b7ac674
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2016-03-24 11:13:55 +0100

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if test -z "$TRAVIS_BRANCH" 
+then
+	TRAVIS_BRANCH=master
+	echo I\'m not on travis-ci
+fi
+
+if test "$TRAVIS_BRANCH" == "master"
+then
+	echo Deploying to GitHub Pages ...
+
+	git checkout gh-pages
+	git pull
+	git add pdf/*.pdf
+	git commit -m "autodeploy to pages"
+	git push
+else
+	echo Not in master branch. I won\'t upload to GitHub Pages
+fi
+
